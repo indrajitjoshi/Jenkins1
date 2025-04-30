fca78023a4d85f6497ff0cb8aac4f9cf58191b09
indrajitjoshi <indrajitjoshi0407@gmail.com>
2025-04-30 10:57:48 +0530

    Create addoftwo.java

---
+++ addoftwo.java
@@ -0,0 +1,13 @@
+public class Addition {
+    public static void main(String[] args) {
+        // Two numbers to be added
+        int num1 = 5;
+        int num2 = 10;
+        
+        // Add the numbers
+        int sum = num1 + num2;
+
+        // Output the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
